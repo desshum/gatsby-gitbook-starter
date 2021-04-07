709e14975e2106fab604c7de319f83fcd9249733
Alexey <alr@digt.ru>
2021-04-07 12:03:28 +0300

    algolia account connection

--- config.js
+++ config.js
@@ -29,10 +29,10 @@ const config = {
     links: [{ text: '', link: '' }],
     search: {
       enabled: true,
-      indexName: '',
-      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
-      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+      indexName: 'Kloud',
+      algoliaAppId: "ALEC0WYN85",
+      algoliaSearchKey: "5b3e9e822ca6424f23ac7eabae6576e9",
+      algoliaAdminKey: "3a662f2a97971b3daa07d1509dc3cf44",
     },
   },
   sidebar: {
